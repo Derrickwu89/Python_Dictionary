c7ef3a3ed5b9c271b24e0b34d886bbe9e2a4f61b
Derrick Wu <derrickwu89@gmail.com>
2018-03-13 19:09:42 -0400

    printing out the definitions

--- app1.py
+++ app1.py
@@ -1,9 +1,22 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def translate(word):
-    return data[word]
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys()))>0:
+        guess = input("Do you mean {}? Please answer Yes or No? ".format(get_close_matches(word, data.keys())[0]))
+        if guess.lower() == "yes":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif guess.lower() == "no":
+            return "Please enter the correct word that you want to define"
+        else:
+            return "I don't understand your answer, please try again later."
+    else:
+        return "The {} cannot be found in the dictionary".format(word)
 
 word = input("Please define: ")
 
